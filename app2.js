82c6c013d3052753e25fd4182d792f7ad9838e46
Peeyush Gupta <peeyush654@gmail.com>
2019-05-26 19:35:36 +0530

    final push

--- app2.js
+++ app2.js
@@ -25,7 +25,7 @@ var chart=Highcharts.chart('container1', {
             label: {
                 connectorAllowed: false
             },
-            //pointStart: 2010
+
         }
     },
 
@@ -111,7 +111,7 @@ var json1={}
             if(k===0){
                 cnt11=cnt11-1
                 //break;
-                alert('state doesnt exist')
+                alert('Data not found in Air pollution dataset')
             }
             if(cnt11>5)
                 alert(`please refresh page`)
@@ -120,7 +120,6 @@ var json1={}
         oReq1.addEventListener("load", reqListener1);
         oReq1.open("GET", "https://api.data.gov.in/resource/53c8db4f-f71f-4f40-a773-4a95d2f9c4ae?api-key=579b464db66ec23bdd0000011f3171c57adb4ebd410ecf3d887ed34a&format=json&offset=0&limit=50");
         oReq1.send();
-        //document.getElementById("graph1").style.display="block"
 
         
 }
@@ -198,7 +197,7 @@ var charts=  Highcharts.chart('container', {
         var v1=0
         var v2=""
         var arr=Array
-        var cnt=0;
+        var cnt=-1;
         arr=[]
         function f(){
             v2=document.getElementById("state").value
@@ -238,7 +237,7 @@ var charts=  Highcharts.chart('container', {
             }
             if(k===0){
                 cnt=cnt-1
-                alert('state doesnt exist')
+                alert('Data not found in Led dataset')
             }
             if(cnt>5)
                 alert(`please refresh page`)
@@ -247,9 +246,8 @@ var charts=  Highcharts.chart('container', {
         oReq.addEventListener("load", reqListener);
         oReq.open("GET", "https://api.data.gov.in/resource/8d308158-28aa-4bcf-9e98-948b029340ad?api-key=579b464db66ec23bdd0000011f3171c57adb4ebd410ecf3d887ed34a&format=json&offset=0&limit=50");
         oReq.send();
-        
-}
-//document.getElementById("graph1").style.display="block"
+        }
+
 var hosjson={}
 function tablef(){
     document.getElementById("mytable").style.display="block"
@@ -257,15 +255,17 @@ function tablef(){
    function reqListener3(){
     hosjson=JSON.parse(this.responseText);
     var state=document.getElementById("host")
-    for(var i=0;i<1048;i++){
-        var cnt=1;
+    var notFound=0
+    for(var i=0;i<1047;i++){
+        var cntHos=1
         if(st.toLowerCase()===hosjson.data[i][6].toLowerCase())
         {
+            notFound=1
             var tbl=document.getElementById("mytable")
-            var row=tbl.insertRow(cnt)
+            var row=tbl.insertRow(cntHos)
             console.log(hosjson.data[i][1])
             console.log(hosjson.data[i][6])
-            cnt=cnt+1;
+            cntHos=cntHos+1;
             var cell1=row.insertCell(0);
             var cell2=row.insertCell(1);
             var cell3=row.insertCell(2);
@@ -277,10 +277,212 @@ function tablef(){
             cell4.innerText=hosjson.data[i][11]
             cell5.innerText=hosjson.data[i][19]
         }
-    }}
+    }
+    if(notFound===0)
+        {
+            alert("Not Found in Hospital Dataset")
+        }
+}
     
     var oReq3 = new XMLHttpRequest();
         oReq3.addEventListener("load", reqListener3);
         oReq3.open("GET", "https://data.gov.in/node/356921/datastore/export/json");
         oReq3.send();
 }
+var chartsw=Highcharts.chart('container2', {
+    chart: {
+        type: 'bar'
+    },
+    title: {
+        text: 'Crime rate against women in 2013 and 2014'
+    },
+    subtitle: {
+        text: 'Source: <a href="https://en.wikipedia.org/wiki/World_population">Wikipedia.org</a>'
+    },
+    xAxis: {
+        categories: ['States', 'States', 'States', 'States', 'States'],
+        title: {
+            text: null
+        }
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Crime rate (in thousands)',
+            align: 'high'
+        },
+        labels: {
+            overflow: 'justify'
+        }
+    },
+    plotOptions: {
+        bar: {
+            dataLabels: {
+                enabled: true
+            }
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'top',
+        x: -40,
+        y: 80,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+        shadow: true
+    },
+    credits: {
+        enabled: false
+    },
+    series: [{
+        name: '',
+        data: []
+    }, {
+        name: '',
+        data: []
+    }, {
+        name: '',
+        data: []
+    }, {
+        name: '',
+        data: []
+    },
+    {
+        name: '',
+        data: []
+    },
+    {
+        name: '',
+        data: []
+    }, {
+        name: '',
+        data: []
+    }, {
+        name: '',
+        data: []
+    }, {
+        name: '',
+        data: []
+    },
+    {
+        name: '',
+        data: []
+    },
+    ]
+});
+
+var jsonwom={}
+        var vw1=0
+        var vw2=""
+        var arrw=Array
+        var cntw=-1;
+        arrw=[]
+        function fw(){
+            vw2=document.getElementById("state").value
+            fw1();
+            fw7();
+        }
+        function fw2(){
+            vw2=document.getElementById("state1").value
+            fw1();
+            fw7();
+        }
+        function fw3(){
+            vw2=document.getElementById("state2").value
+            fw1();
+            fw7();
+        }
+        function fw4(){
+            vw2=document.getElementById("state3").value
+            fw1();
+            fw7();
+        }
+        function fw5(){
+            vw2=document.getElementById("state4").value
+            fw1();
+            fw7();
+        }
+        var tempw=0
+        function fw1(){
+            document.getElementById("graphsw").style.display="flex";
+        function reqListenerw () {
+        jsonwom=JSON.parse(this.responseText);
+          ///  let kw=0
+          tempw=0
+            for(var i=0;i<1559;i++){
+               
+            if(jsonwom.data[i][0].toLowerCase()===v2.toLowerCase()){
+                //kw=1
+                vw1=parseInt(jsonwom.data[i][2])
+                tempw=vw1
+                //arrw.push([v2,vw1])
+                console.log(`vw1 is ${vw1}`)
+                //cntw=cntw+1
+                
+            
+            }
+            }
+            console.log(`tempw ${tempw}`)
+            if(tempw>0)
+            {
+                cntw=cntw+1
+                chartsw.series[cntw].addPoint([v2,tempw/1000]);
+            }
+           // vw1=tempw1
+           if(tempw===0)
+           {
+            alert('Data not found in Crime against women 2014!!')
+           }
+            if(cntw>11){
+                alert('refresh page!!')
+            }
+
+        //fw7();
+
+    }
+        var oReqw = new XMLHttpRequest();
+        oReqw.addEventListener("load", reqListenerw);
+        oReqw.open("GET", "https://data.gov.in/node/681441/datastore/export/json");
+        oReqw.send();
+        }
+        var sum7=0
+        function fw7(){
+            document.getElementById("graph2").style.display="flex";
+        function reqListenerw7 () {
+        jsonwom7=JSON.parse(this.responseText);
+            var kw7=0
+            sum7=0
+            for(var i=0;i<455;i++){
+            if(jsonwom7.data[i][0].toLowerCase()===v2.toLowerCase()){
+                kw7=1
+                sum7=sum7+parseInt(jsonwom7.data[i][2])
+                
+                console.log(`i of 2 is ${jsonwom7.data[i][2]}`)
+                //cntw=cntw+1
+                //chartsw.series[cntw].addPoint([vw2,vw7])
+                //break
+            }
+            }
+            console.log(`kw7 is ${kw7}`)
+            if(kw7===1){
+                //var dataset=[vw1,sum];
+                cntw=cntw+1
+                console.log(`sum 7 is ${sum7}`)
+                chartsw.series[cntw].addPoint([v2,sum7/1000]);
+                
+            }
+            if(kw7===0){
+                cntw=cntw-1
+                alert('Data not found in crime against women 2013')
+            }
+            if(cntw>10){
+                alert(`please refresh page`)}
+            console.log(jsonwom7)}
+        var oReqw7 = new XMLHttpRequest();
+        oReqw7.addEventListener("load", reqListenerw7);
+        oReqw7.open("GET", "https://data.gov.in/node/147923/datastore/export/json");
+        oReqw7.send();
+            }
+        
